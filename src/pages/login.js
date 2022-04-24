4c28e416cfd288addbe019d9c4d7430d0a1605e3
harshlg929 <harshlg929@gmail.com>
2022-04-24 17:41:01 +0530

    Login page creation and component creation

---
+++ src/pages/login.js
@@ -0,0 +1,12 @@
+class Login extends Component {
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
